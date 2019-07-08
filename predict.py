d23e44a29b2303b26b0752b3afa148aba15ad969
Dennis SH Lai <dshlai@gmail.com>
2019-07-08 22:19:20 +0800

    * Add more verbose output for evaluate_model * refactor pre_processing

--- predict.py
+++ predict.py
@@ -11,17 +11,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import cross_val_score
 
-
 training_path = "occupancy_data/datatraining.txt"
 test_path = "occupancy_data/datatest.txt"
 test2_path = "occupancy_data/datatest2.txt"
 
 
-def pre_processing():
-    train_data = pd.read_csv(training_path)
-    test2_data = pd.read_csv(test2_path)
-
-    data = pd.concat([train_data, test2_data], axis=0)
+def pre_processing(path):
+    data = pd.read_csv(path)
     data.drop('date', axis=1, inplace=True)
     data.drop('HumidityRatio', axis=1, inplace=True)
 
@@ -32,16 +28,18 @@ def pre_processing():
     return X, y
 
 
-def load_test():
-    test_data = pd.read_csv(test_path)
-    test_data.drop('date', axis=1, inplace=True)
-    test_data.drop('HumidityRatio', axis=1, inplace=True)
+def load_train():
+    train_x, train_y = pre_processing(training_path)
+    test2_x, test2_y = pre_processing(test2_path)
 
-    values = test_data.values
-
-    X, y = values[:, :-1], values[:, -1]
+    X = np.concatenate([train_x, test2_x], axis=0)
+    y = np.concatenate([train_y, test2_y], axis=0)
 
     return X, y
+
+
+def load_test():
+    return pre_processing(test_path)
 
 
 def fit_model(train_x, train_y, model=None):
@@ -54,13 +52,14 @@ def fit_model(train_x, train_y, model=None):
     else:
         model_ = LogisticRegression(solver='lbfgs')
 
+    print("\nFit using \'{}\'\n".format(model))
     model_.fit(train_x, train_y)
 
     return model_
 
 
 def evaluate_model(model_str):
-    X, y = pre_processing()
+    X, y = load_train()
     test_x, test_y = load_test()
 
     train_x, valid_x, train_y, valid_y = train_test_split(X, y, test_size=0.25, shuffle=False, random_state=1)
@@ -71,6 +70,7 @@ def evaluate_model(model_str):
     y_hat_test = model.predict(test_x)
 
     # Calculate ROC-AUC score
+
     y_pred_prob = model.predict_proba(test_x)[:, 1]
     auc_score = roc_auc_score(test_y, y_pred_prob)
 
@@ -81,12 +81,11 @@ def evaluate_model(model_str):
     validation_accuracy = accuracy_score(valid_y, y_hat_valid)
     test_accuracy = accuracy_score(test_y, y_hat_test)
 
-    print("Validation Accuracy: {}\nTest Accuracy: {}\nROC-AUC Score: {}\nMean CV Scores: {}".format(
-            validation_accuracy, test_accuracy, auc_score, mean_cv))
+    print("Validation Accuracy: {}\nTest Accuracy: {}\nROC-AUC Score: {}\nMean CV Scores: {}\n".format(
+        validation_accuracy, test_accuracy, auc_score, mean_cv))
 
 
 if __name__ == "__main__":
-
     help_str = "Selecta model to evaluation, choices are: "
     help_str += "\'Logistic\', \'GBT (Gradient Boost Tree)\', and \'RF (Random Forest)\'"
 
