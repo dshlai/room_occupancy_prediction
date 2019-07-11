f0821d56c4c9208815f7f3478c63e7e8e6f7a800
Dennis SH Lai <dshlai@gmail.com>
2019-07-11 22:54:03 +0800

    * rename imbalanced_predict to imbalanced_model_evaluation * add SMOTENEE function to predict.py * add more detail about imbalance in room_occupancy

--- predict.py
+++ predict.py
@@ -8,8 +8,11 @@ from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, balanced_accuracy_score
 from sklearn.model_selection import cross_val_score
+
+from imblearn.ensemble import BalancedBaggingClassifier
+from imblearn.combine import SMOTEENN
 
 training_path = "occupancy_data/datatraining.txt"
 test_path = "occupancy_data/datatest.txt"
@@ -26,6 +29,14 @@ def pre_processing(path):
     X, y = values[:, :-1], values[:, -1]
 
     return X, y
+
+
+def smoteenn(x, y):
+
+    smote_enn = SMOTEENN(random_state=0)
+    X_resamp, y_resamp = smote_enn.fit_resample(x, y)
+
+    return X_resamp, y_resamp
 
 
 def load_train():
@@ -46,9 +57,11 @@ def fit_model(train_x, train_y, model=None):
     if model == 'GBT':
         model_ = GradientBoostingClassifier()
     elif model == 'RF':
-        model_ = RandomForestClassifier(n_estimators=1250, n_jobs=4)
+        model_ = RandomForestClassifier(n_estimators=250, n_jobs=-1)
     elif model == 'Logistic':
         model_ = LogisticRegression(solver='lbfgs')
+    elif model == 'BalancedBag':
+        model_ = BalancedBaggingClassifier(n_estimators=250, n_jobs=-1)
     else:
         model_ = LogisticRegression(solver='lbfgs')
 
@@ -60,9 +73,11 @@ def fit_model(train_x, train_y, model=None):
 
 def evaluate_model(model_str):
     X, y = load_train()
+    X_resampled, y_resampled = smoteenn(X, y)
     test_x, test_y = load_test()
 
-    train_x, valid_x, train_y, valid_y = train_test_split(X, y, test_size=0.25, shuffle=False, random_state=1)
+    train_x, valid_x, train_y, valid_y = train_test_split(X_resampled, y_resampled,
+                                                          test_size=0.25, shuffle=False, random_state=1)
 
     model = fit_model(train_x, train_y, model=model_str)
 
@@ -71,8 +86,8 @@ def evaluate_model(model_str):
 
     # Calculate ROC-AUC score
 
-    y_pred_prob = model.predict_proba(test_x)[:, 1]
-    auc_score = roc_auc_score(test_y, y_pred_prob)
+    y_pred_prob = model.predict_proba(X)[:, 1]
+    auc_score = roc_auc_score(y, y_pred_prob)
 
     # AUC with CV
     cv_scores = cross_val_score(model, X, y, cv=10)
@@ -81,12 +96,15 @@ def evaluate_model(model_str):
     validation_accuracy = accuracy_score(valid_y, y_hat_valid)
     test_accuracy = accuracy_score(test_y, y_hat_test)
 
-    print("Validation Accuracy: {}\nTest Accuracy: {}\nROC-AUC Score: {}\nMean CV Scores: {}\n".format(
-        validation_accuracy, test_accuracy, auc_score, mean_cv))
+    balanced_accuracy_valid = balanced_accuracy_score(valid_y, y_hat_valid)
+    balanced_accuracy_test = balanced_accuracy_score(test_y, y_hat_test)
+
+    print("Validation Accuracy: {}\nTest Accuracy: {}\nBalanced Valid Accu.: {}\nBalanced Test Accu.: {}\nROC-AUC Score: {}\nMean CV Scores: {}\n".format(
+        validation_accuracy, test_accuracy, balanced_accuracy_valid, balanced_accuracy_test, auc_score, mean_cv))
 
 
 if __name__ == "__main__":
-    help_str = "Selecta model to evaluation, choices are: "
+    help_str = "Select a model to evaluation, choices are: "
     help_str += "\'Logistic\', \'GBT (Gradient Boost Tree)\', and \'RF (Random Forest)\'"
 
     parser = argparse.ArgumentParser()
